fbc0383dde329b80b0ace8adbce8df6992aac94e
Tapiocaman <acore@uci.edu>
2016-12-31 20:08:15 -0800

    deleting old project

--- DPOApp/config/routes.rb
+++
@@ -1,7 +0,0 @@
-Rails.application.routes.draw do
-  resources :rooms
-  get 'home/index'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'application#hello'
-end
